9e9a25e33c07b9d29d8c53df8fd49e8c79620cee
Xafiq <148711588+Xafiq@users.noreply.github.com>
2024-09-25 01:52:33 +0200

    Create copyspecial.py

---
+++ copyspecial/copyspecial.py
@@ -0,0 +1,36 @@
+import sys
+import re
+import os
+import shutil
+import subprocess
+
+"""Copy Special exercise
+"""
+
+# +++your code here+++
+# Write functions and modify main() to call them
+
+
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print('usage: [--todir dir][--tozip zipfile] dir [dir ...]')
+    sys.exit(1)
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  tozip = ''
+  if args[0] == '--tozip':
+    tozip = args[1]
